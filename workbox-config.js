7d6c19aca6131195e8a5690202a8a3486bf2b3d4
arthurR3 <hedzjose014@gmail.com>
2024-11-05 09:48:48 -0600

    modified:   src/pages/public/Login/Login.js 	modified:   src/serviceWorkerRegistration.js 	modified:   src/test/servicios.test.js 	modified:   workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,64 +1,66 @@
 module.exports = {
-    globDirectory: "build/",
-    globPatterns: [
+  globDirectory: "build/",
+  globPatterns: [
       "**/*.{html,js,css,png,jpg,json,ico}"
-    ],
-    swDest: "build/service-worker.js",
-    runtimeCaching: [
+  ],
+  swDest: "build/service-worker.js",
+  runtimeCaching: [
       {
-        urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "images-cache",
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 días
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+          handler: "CacheFirst",
+          options: {
+              cacheName: "images-cache",
+              expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 30 días
+              },
           },
-        },
       },
       {
-        urlPattern: /\.(?:js|css)$/,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-resources',
-          expiration: {
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 días
+          urlPattern: /\.(?:js|css)$/,
+          handler: 'StaleWhileRevalidate',
+          options: {
+              cacheName: 'static-resources',
+              expiration: {
+                  maxAgeSeconds: 7 * 24 * 60 * 60, // 7 días
+              },
           },
-        },
       },
       {
-        urlPattern:/https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/products/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'products',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 5 * 60, // 5 minutos
+          urlPattern: /https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/products/,
+          handler: 'NetworkFirst', // Cambia esto a CacheFirst si quieres acceso offline más confiable
+          options: {
+              cacheName: 'products',
+              expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 5 * 60, // 5 minutos
+              },
+              networkTimeoutSeconds: 5, // Tiempo para esperar la respuesta antes de usar la caché
           },
-        },
       },
       {
-        urlPattern: /https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/services/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'services',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 5 * 60, // 5 minutos
+          urlPattern: /https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/services/,
+          handler: 'NetworkFirst', // Cambia esto a CacheFirst si quieres acceso offline más confiable
+          options: {
+              cacheName: 'services',
+              expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 5 * 60, // 5 minutos
+              },
+              networkTimeoutSeconds: 5, // Tiempo para esperar la respuesta antes de usar la caché
           },
-        },
       },
       {
-        urlPattern: /https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/users\/\d+/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'user-detail',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 5 * 60, // 5 minutos
+          urlPattern: /https:\/\/back-estetica-production-e475\.up\.railway\.app\/api\/v1\/users\/\d+/,
+          handler: 'NetworkFirst', // Cambia esto a CacheFirst si quieres acceso offline más confiable
+          options: {
+              cacheName: 'user-detail',
+              expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 5 * 60, // 5 minutos
+              },
+              networkTimeoutSeconds: 5, // Tiempo para esperar la respuesta antes de usar la caché
           },
-        },
       },
-    ],
-  };
-  
+  ],
+};

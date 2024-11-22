d5b47f07491f6ae85bd0e666685fc62d6ddde69f
arthurR3 <hedzjose014@gmail.com>
2024-11-22 17:50:07 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -9,23 +9,31 @@ import App from './App';
 import { AuthProvider } from './Components/Context/AuthContext';
 import { AdminAuthProvider } from './Components/Context/AdminAuthContext';
 import CarritoContext from './Components/Context/CarritoContext';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration.js'
-import * as notification from './notification.js'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
+import * as notification from './notification.js';
+
+// Condicional para importar Analytics si está en Vercel
+let AnalyticsComponent = () => null;
+if (process.env.VERCEL) {
+  const { Analytics } = require("@vercel/analytics/react");
+  AnalyticsComponent = Analytics;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
-        <CarritoContext>
-          <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
-            <App />
-          </PrimeReactProvider>
-        </CarritoContext>
+      <CarritoContext>
+        <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+          <App />
+          <AnalyticsComponent /> {/* Solo se renderiza en Vercel */}
+        </PrimeReactProvider>
+      </CarritoContext>
     </AuthProvider>
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register()
-notification.requestNotificationPermission()
+serviceWorkerRegistration.register();
+notification.requestNotificationPermission();
 reportWebVitals();
